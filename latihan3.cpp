49f5378b1e7f34dcfc878d25bdf54706d6b6ee3c
AbdulMalik040414 <44117281+AbdulMalik040414@users.noreply.github.com>
2018-10-13 21:04:38 +0700

    Add files via upload

---
+++ latihan3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main()
+{
+	int a,b,c;
+
+	std::cout << "masukan nilai pertama : ";
+	std::cin >>a;
+	std::cout << "masukan nilai kedua : ";
+	std::cin >>b;
+	std::cout << "masukan nilai ketiga : ";
+	std::cin >>c;
+
+	if (a>b)
+	{
+		if(a>c)
+			std::cout << "bilangan terbesar adalah : " << a;
+		else
+			std::cout << "bilangan terbesar adalah : " << c;
+	}
+
+	else
+	{
+		if(b>c)
+			std::cout << "bilangan terbesar adalah : " << b;
+		else
+			std::cout << "bilangan terbesar adalah : " << c;
+	}
+	return 0;
+}
